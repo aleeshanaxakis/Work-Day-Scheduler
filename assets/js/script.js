55c84f4efd2644ea9dd1333ff4880638c5667286
Aleesha Naxakis <aleesha.naxakis@cancer.org.au>
2023-09-21 17:22:42 +1000

    compared currentTime to id on all rows- third acceptance criterion

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,30 @@ $(document).ready(function() {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+
+    function colourCoding() {
+    // Get the current hour using Day.js
+        var currentTime = dayjs().hour();
+    // Iterate over each row
+        $('.row').each(function () {
+            var row = $(this);
+            var id = parseInt(row.attr('id').replace('hour-', ''));
+
+            row.removeClass("past present future");
+    
+    // Compare currentTime with id to determine the colour
+        if (currentTime < id) {
+            row.addClass("future");
+        } else if (currentTime === id) {
+            row.addClass("present");
+        } else {
+            row.addClass("past");
+        }
+        });
+    }
+
+    colourCoding()
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
